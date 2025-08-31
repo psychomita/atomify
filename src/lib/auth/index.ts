31d418a1773308340d05e87a6217e96880cf2314
Suchismita Bose <133328192+psychomita@users.noreply.github.com>
2025-08-31 21:50:13 +0530

    feat: implement authentication client and session management with BetterAuth

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,35 @@
+import prisma from "@/lib/prisma";
+import { betterAuth, BetterAuthOptions } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "mongodb",
+  }),
+  plugins: [nextCookies()],
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24, // 1 day (every 1 day the session expiration is updated)
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // Cache duration in seconds
+    },
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: false,
+      },
+    },
+  },
+} satisfies BetterAuthOptions);
+
+export type Session = typeof auth.$Infer.Session;
